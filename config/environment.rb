e0662bad17a3d06c1816be8b675663140051808c
Matthew Licausi <matthewlicausi@gmail.com>
2014-07-07 10:34:17 -0500

    change to beaus number

--- config/environment.rb
+++ config/environment.rb
@@ -8,5 +8,5 @@ ACCOUNT_SID = 'AC67466026613a30f96c4b466d707ea73c'
 ACCOUNT_TOKEN = 'd56f9c2ce1668c83a4fec65c8e2c4d6e'
 BASE_URL = "http://clicktocall.harvestresourcegroup.com/twilio/clicktocall/"
 TWILIO_NUMBER = "+19133533367"
-SERVICE_PROVIDER_NUMBER = "+18168988132"
+SERVICE_PROVIDER_NUMBER = "+19132198421"
 REDIRECT_URL = "http://harvestresourcegroup.com/contact"
