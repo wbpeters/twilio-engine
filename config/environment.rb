9bb4f59c5af04f244cb75e8888bf1c42b5c03e6d
Matthew Licausi <matthewlicausi@gmail.com>
2014-07-07 09:45:37 -0500

    add return url

--- config/environment.rb
+++ config/environment.rb
@@ -9,3 +9,4 @@ ACCOUNT_TOKEN = 'd56f9c2ce1668c83a4fec65c8e2c4d6e'
 BASE_URL = "http://clicktocall.harvestresourcegroup.com/twilio/clicktocall/"
 TWILIO_NUMBER = "+19133533367"
 SERVICE_PROVIDER_NUMBER = "+18168988132"
+REDIRECT_URL = "http://harvestresourcegroup.com/contact"
