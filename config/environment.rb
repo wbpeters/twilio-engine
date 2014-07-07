8d0a7488f6db10fb505f701add8b75f16afb9d29
Matthew Licausi <matthewlicausi@gmail.com>
2014-07-07 08:15:01 -0500

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+ACCOUNT_SID = 'AC67466026613a30f96c4b466d707ea73c'
+ACCOUNT_TOKEN = 'd56f9c2ce1668c83a4fec65c8e2c4d6e'
+BASE_URL = "http://clicktocall.harvestresourcegroup.com/twilio/clicktocall/"
+TWILIO_NUMBER = "+19133533367"
+SERVICE_PROVIDER_NUMBER = "+18168988132"
